fa3649879bdb86dafc942000cf2278e4dccd0f5b
Lovelex <lukas489@gmail.com>
2021-04-07 01:49:04 -0300

    add: anchor to facebook

--- src/layout/main/footer/Footer.vue
+++ src/layout/main/footer/Footer.vue
@@ -1,17 +1,25 @@
 <template>
   <v-footer elevation="2" fixed absolute>
     <v-sheet width="100%" class="text-center">
-      <span class="deep-orange--text font-weight-medium">Criado por Fire Stacks</span>
+      <span class="deep-orange--text font-weight-medium"
+        >Criado por Fire
+        <v-btn
+          href="https://www.facebook.com/firestacks"
+          target="blank"
+          color="orange darken-4"
+          text
+          class="font-weight-black"
+        >
+          Firestacks
+        </v-btn>
+      </span>
     </v-sheet>
   </v-footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
